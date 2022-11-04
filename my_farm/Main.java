69e441e9b515c560a871294b08584d3bd93e2d84
ReignLarraquel <reignlarraquel@gmail.com>
2022-11-05 00:11:33 +0800

    feat: init main java class

---
+++ my_farm/Main.java
@@ -0,0 +1,14 @@
+package my_farm;
+
+public class Main {
+   public static void main(String[] args) {
+      // Farmer farmer = new Farmer();
+
+      // //Setup Map:
+      // Shop shop = new Shop();
+      // UseItem useItem = new UseItem();
+      Farm farm = new Farm();
+
+      farm.printOut();
+   }
+}
