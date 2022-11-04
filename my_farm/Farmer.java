a40719965f79a690c5e7b5f8dad8b0cf2bef5ab1
ReignLarraquel <reignlarraquel@gmail.com>
2022-11-04 19:26:43 +0800

    feat: add constructor, getters, and setters

--- my_farm/Farmer.java
+++ my_farm/Farmer.java
@@ -1,7 +1,40 @@
 package my_farm;
+
+import java.util.ArrayList;
+
 public class Farmer {
    private int objectCoins;
    private Level level;
+   private ArrayList<Seeds> seeds;
 
+   public Farmer(int objectCoins, Level level) {
+      this.objectCoins = objectCoins;
+      this.level = level;
+      this.seeds = new ArrayList<Seeds>();
+   }
+
+   public int getObjectCoins() {
+      return objectCoins;
+   }
+
+   public void setObjectCoins(int objectCoins) {
+      this.objectCoins = objectCoins;
+   }
+
+   public Level getLevel() {
+      return level;
+   }
+
+   public void setLevel(Level level) {
+      this.level = level;
+   }
+
+   public ArrayList<Seeds> getSeeds() {
+      return seeds;
+   }
+
+   public void addSeeds(Seeds seed) {
+      seeds.add(seed);
+   }
 
 }
