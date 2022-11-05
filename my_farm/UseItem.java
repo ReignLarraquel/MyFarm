7824f7a9a1c6967a6ef95544d418086ed7385188
ReignLarraquel <reignlarraquel@gmail.com>
2022-11-05 23:23:25 +0800

    feat: add use method

--- my_farm/UseItem.java
+++ my_farm/UseItem.java
@@ -15,5 +15,37 @@ public class UseItem {
       this.tools.add(new Tools("Shovel", 7, 2));
    }
 
-   
+   public void use(Farmer farmer, String toolName, int x, int y) {
+      for (Tools tool : this.tools) {
+         if (toolName == tool.getToolName()) {
+            if (farmer.getObjectCoins() >= tool.getUsageCost()) {
+               farmer.setObjectCoins(farmer.getObjectCoins() - tool.getUsageCost());
+               
+               switch(toolName) {
+                  case "Plow":
+                     Farm.setTile(x, y, "[_]");
+                     Farm.changeTileState(x, y, "Plowed");
+                     break;
+                  case "Watering Can":
+                     Farm.changeTileState(x, y, "Watered");
+                     break;
+                  case "Fertilizer":
+                     Farm.changeTileState(x, y, "Fertilized");
+                     break;
+                  case "Pickaxe":
+                     Farm.setTile(x, y, "[ ]");
+                     Farm.changeTileState(x, y, "Unplowed");
+                     break;
+                  case "Shovel":
+                     Farm.setTile(x, y, "[ ]");
+                     Farm.changeTileState(x, y, "Unplowed");
+                     break;
+               } 
+
+            } else {
+               System.out.println("You do not have enough coins to use a" + tool.getToolName() + ".");
+            }
+         }
+      }
+   }
 }
