536df8403812ef422d865a43436fc428c578129c
ReignLarraquel <reignlarraquel@gmail.com>
2022-11-07 03:46:37 +0800

    feat: add toolMenu and call to waterPlant method

--- my_farm/UseItem.java
+++ my_farm/UseItem.java
@@ -1,6 +1,7 @@
 package my_farm;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class UseItem {
    private ArrayList<Tools> tools;
@@ -8,7 +9,7 @@ public class UseItem {
    public UseItem() {
       this.tools = new ArrayList<Tools>();
 
-      this.tools.add(new Tools("Plow", 0, 0.5));
+      this.tools.add(new Tools("Plow", 0.0, 0.5));
       this.tools.add(new Tools("Watering Can", 0, 0.5));
       this.tools.add(new Tools("Fertilizer", 10, 4));
       this.tools.add(new Tools("Pickaxe", 50, 15));
@@ -28,7 +29,7 @@ public class UseItem {
                      Farm.changeTileState(x, y, "Plowed");
                      break;
                   case "Watering Can":
-                     Farm.changeTileState(x, y, "Watered");
+                     Farm.waterPlant(farmer, x, y);
                      break;
                   case "Fertilizer":
                      Farm.changeTileState(x, y, "Fertilized");
@@ -42,11 +43,46 @@ public class UseItem {
                      Farm.changeTileState(x, y, "Unplowed");
                      break;
                } 
-
-            } else {
+            } 
+            else {
                System.out.println("You do not have enough coins to use a" + tool.getToolName() + ".");
             }
          }
+      }
+   }
+
+   public void toolMenu(int choice, Farmer farmer){
+      Scanner input = new Scanner(System.in);
+      int tool = 0;
+      int x;
+      int y;
+
+      System.out.println("\nTOOLS:\n [1]Plow\t\t [4]Pickaxe\n [2]Watering Can\t [5]Shovel\n [3]Fertilizer");
+      tool = input.nextInt();
+      
+      System.out.println("\n" + getTools().get(choice - 1).getToolName().toUpperCase());
+      System.out.println("Enter the coordinates of the tile you wish to use the tool: ");
+      System.out.print("X: ");
+      x = input.nextInt();
+      System.out.print("Y: ");
+      y = input.nextInt();
+
+      switch(tool){
+         case 1:
+            use(farmer, "Plow", x, y);
+            break;
+         case 2:
+            use(farmer, "Watering Can", x, y);
+            break;
+         case 3:
+            use(farmer, "Fertilizer", x, y);
+            break;
+         case 4:
+            use(farmer, "Pickaxe", x, y);
+            break;
+         case 5:
+            use(farmer, "Shovel", x, y);
+            break;
       }
    }
 
