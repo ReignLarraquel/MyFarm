54a344246d96d2b3b463b84294eba8533ce614d4
ReignLarraquel <reignlarraquel@gmail.com>
2022-11-06 14:54:48 +0800

    refactor: Set to MCO1 requirements and add checkTileState
    MCO1 requirements: There is only 1 tile

--- my_farm/Farm.java
+++ my_farm/Farm.java
@@ -16,33 +16,51 @@ public class Farm {
    private static ArrayList<Tiles> tileList;
 
    public Farm() {
-      Farm.tiles = new String[5][10];
+      Farm.tiles = new String[1][1]; // Change to 5x10 after MCO1
       Farm.tileList = new ArrayList<Tiles>();
-      for(int i = 0; i < 5; i++) {
-         for(int j = 0; j < 10; j++) {
-            tiles[i][j] = "[ ]";
-            tileList.add(new Tiles(i, j, "Unplowed"));
-         }
-      }
+
+      //Remove after MCO1
+      tiles[0][0] = "[ ]";
+      tileList.add(new Tiles(0, 0, "Unplowed"));
+
+      //Uncomment after MCO1
+      // Farm.tiles = new String[5][10];
+      // for(int i = 0; i < 5; i++) {
+      //    for(int j = 0; j < 10; j++) {
+      //       tiles[i][j] = "[ ]";
+      //       tileList.add(new Tiles(i, j, "Unplowed"));
+      //    }
+      // }
    }
 
    public void displayFarm() {
-      System.out.println(" ___________________________________________");
-      System.out.println("/                                           \\");
+      System.out.println("\n\t\t" + tiles[0][0]);
 
-      for(int i = 0; i < 5; i++) {
-         System.out.print("|  ");
-         for(int j = 0; j < 10; j++) {
-            System.out.print(tiles[i][j] + " ");
-         }
-         System.out.print(" |");
-         System.out.println();
-      }
-      System.out.println("\\___________________________________________/");
+      // Uncomment once MCO1 is complete
+      // System.out.println(" ___________________________________________");
+      // System.out.println("/                                           \\");
+
+      // for(int i = 0; i < 5; i++) {
+      //    System.out.print("|  ");
+      //    for(int j = 0; j < 10; j++) {
+      //       System.out.print(tiles[i][j] + " ");
+      //    }
+      //    System.out.print(" |");
+      //    System.out.println();
+      // }
+      // System.out.println("\\___________________________________________/");
    }
 
    public static void setTile(int x, int y, String tile) {
       tiles[x][y] = tile;
+   }
+
+   public void checkTileState(int x, int y) {
+      for(Tiles tile : tileList) {
+         if(tile.getX() == x && tile.getY() == y) {
+            System.out.println(tile.getTileState());
+         }
+      }
    }
 
    public static void changeTileState(int x, int y, String tileState) {
@@ -63,15 +81,18 @@ public class Farm {
       x = input.nextInt();
       System.out.println("Y: ");
       y = input.nextInt();
+      //if(Farm.getTileState(x, y).equals("Unplowed")) { //Fix to check tileState if plowed
+            Farm.setTile(x, y, "[v]");
+            Farm.changeTileState(x, y, "Planted");
 
-      Farm.setTile(x, y, "[v]");
-      Farm.changeTileState(x, y, "Planted");
+            for(Tiles tile : Farm.tileList) { //This may or may not be error but too lazy to checkS
+               if(tile.getX() == x && tile.getY() == y) {
+                  tile.setSeed(seed);
+               }
+            }
+      //}
 
-      for(Tiles tile : Farm.tileList) { //This may or may not be error but too lazy to checkS
-         if(tile.getX() == x && tile.getY() == y) {
-            tile.setSeed(seed);
-         }
-      }
+
    }
 
 }
