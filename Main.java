6a1bc9f611aef1b3bdd4900e894ed75c0cde1f2c
ReignLarraquel <reignlarraquel@gmail.com>
2022-11-07 16:44:25 +0800

    refactor: move method from main to game
    moved: menu and monologue

--- Main.java
+++ Main.java
@@ -5,35 +5,6 @@ public class Main {
    public static int gameStatus = 0;
    public static int day = 0;
 
-   public static void menu(){
-      Scanner input = new Scanner(System.in);
-      day = 0;
-
-      System.out.println("Welcome to the Farm!\n");
-      System.out.print("[1] Start a new Game\n[2] Exit Game\n\nEnter your choice: ");
-      gameStatus = input.nextInt();
-   }
-
-   public static String monologue(){
-      Scanner input = new Scanner(System.in);
-      Scanner key = new Scanner(System.in);
-
-      String name = "";
-
-      System.out.println("\nHey you, you're finally awake.");
-      System.out.println("What is your name?");
-      name = input.next();
-
-      System.out.print("\nHello " + name + "!\nWell, what do you think? This was your grandpa's farm. (Enter to continue)");
-      key.nextLine();
-      System.out.print("It's been neglected for quite some time, but I'm sure you can bring it back to life. (Enter to continue)");
-      key.nextLine();
-      System.out.print("I'll be here to help you along the way. (Enter to continue)");
-      key.nextLine();
-
-      return name;
-   }
-
    public static void main(String[] args) {
       Farm farm = new Farm();
       Farmer farmer;
@@ -62,10 +33,15 @@ public class Main {
 
             game.header(farmer, farm);
 
-            if(game.gameOver(farm) == 1) {
+            if(game.gameOver(farm, farmer) == 1) {
                gameStatus = 2;
                advanceDay = 1;
                System.out.println("\nAll your plants have withered...\n\n[YOU LOSE]");
+            }
+            else if(game.gameOver(farm, farmer) == 2) {
+               gameStatus = 2;
+               advanceDay = 1;
+               System.out.println("\nYou have no more coins...\n\n[YOU LOSE]");
             }
             else{
                choice = game.footer();
@@ -97,9 +73,9 @@ public class Main {
                   advanceDay = 1;
                }
                if(choice == 5) {
-                  menu();
+                  gameStatus = game.menu();
                   advanceDay = 1;
-                  if(gameStatus == 1) farmer = new Farmer(monologue());
+                  if(gameStatus == 1) farmer = new Farmer(game.monologue());
                }
    
             }
