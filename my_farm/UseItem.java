a11e8badf9c907ca9900f9aa2e157591c0aca3af
ReignLarraquel <reignlarraquel@gmail.com>
2022-11-23 02:24:54 +0800

    feat: create method for each tool
    tools: plow, wateringcan, fertilizer, pickaxe, and shovel

--- my_farm/UseItem.java
+++ my_farm/UseItem.java
@@ -1,5 +1,15 @@
 package my_farm;
 
+/*
+ * Tile States:
+ * Plow:          [_]
+ * Unplowed:      [ ]
+ * Rock:          [¤]
+ * Planted:       [v]
+ * Must Harvest:  [Y]
+ * Withered:      […]
+*/
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -35,22 +45,19 @@ public class UseItem {
                
                switch(toolName) {
                   case "Plow":
-                     Farm.setTile(x, y, "[_]");
-                     Farm.changeTileState(x, y, "Plowed");
+                     plow(x, y);
                      break;
                   case "Watering Can":
-                     waterPlant(farmer, x, y);
+                     wateringCan(x, y);
                      break;
                   case "Fertilizer":
-                     //Add fertilizer code here
+                     fertilizer(x, y);
                      break;
                   case "Pickaxe":
-                     Farm.setTile(x, y, "[ ]");
-                     Farm.changeTileState(x, y, "Unplowed");
+                     pickaxe(x, y);
                      break;
                   case "Shovel":
-                     Farm.setTile(x, y, "[ ]");
-                     Farm.changeTileState(x, y, "Unplowed");
+                     shovel(x, y);
                      break;
                } 
             } 
@@ -108,6 +115,18 @@ public class UseItem {
       }
    }
 
+   public void plow(int x, int y){
+      for(Tiles tile : Farm.getTileList()) {
+         if(tile.getTileState() == "Unplowed") {
+            Farm.setTile(x, y, "[_]");
+            Farm.changeTileState(x, y, "Plowed");
+         }
+         else{
+            System.out.println("You cannot plow this tile.");
+         }
+      }
+   }
+
       /**
     * It counts the number of times the plant is watered
     * 
@@ -115,7 +134,7 @@ public class UseItem {
     * @param x the x coordinate of the tile
     * @param y The y coordinate of the tile you want to water.
     */
-   public static void waterPlant(Farmer farmer, int x, int y) {
+   public static void wateringCan(int x, int y) {
       for(Tiles tile : Farm.getTileList()) {
          if(tile.getX() == x && tile.getY() == y) {
             if(tile.getTileState() == "Planted") {
@@ -127,7 +146,66 @@ public class UseItem {
 
          }
       }
+   } 
+   
+   public static void fertilizer(int x, int y) {
+      for(Tiles tile : Farm.getTileList()) {
+         if(tile.getX() == x && tile.getY() == y) {
+            if(tile.getTileState() == "Planted") {
+               tile.setFertilizerCount(1);
+               System.out.println("You fertilized the plant " + tile.getFertilizerCount() + " times.");
+            } else {
+               System.out.println("There is no plant to fertilize.");
+            }
+
+         }
+      }
    }  
+
+   public void shovel(int x, int y) {
+      for(Tiles tile : Farm.getTileList()) {
+         if(tile.getX() == x && tile.getY() == y) {
+            if(tile.getTileState() == "Planted" || tile.getTileState() == "Must Harvest") {
+               tile.setTileState("Unplowed");
+               Farm.setTile(x, y, "[ ]");
+               System.out.println("You dug up the plant.");
+            }
+            else if(tile.getTileState() == "Rock") {
+               System.out.println("You cannot dig up a rock.");
+            }
+            else if(tile.getTileState() == "Withered") {
+               tile.setTileState("Unplowed");
+               Farm.setTile(x, y, "[ ]");
+               System.out.println("You dug up the withered plant.");
+            }
+            else if (tile.getTileState() == "Unplowed") {
+               tile.setTileState("Unplowed");
+               Farm.setTile(x, y, "[ ]");
+               System.out.println("You dug up.. dirt.");
+            }
+            else if (tile.getTileState() == "Plowed") {
+               tile.setTileState("Unplowed");
+               Farm.setTile(x, y, "[ ]");
+               System.out.println("You dug up.. dirt.");
+            }
+         }
+      }
+   }
+
+   public void pickaxe(int x, int y) {
+      for(Tiles tile : Farm.getTileList()) {
+         if(tile.getX() == x && tile.getY() == y) {
+            if(tile.getTileState() == "Rock") {
+               tile.setTileState("Unplowed");
+               Farm.setTile(x, y, "[ ]");
+               System.out.println("You got rid of a rock.");
+            }
+            else {
+               System.out.println("You did nothing..");
+            }
+         }
+      }
+   }
       
    /**
     * This function returns the tools array list
